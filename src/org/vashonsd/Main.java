6c99b19c37582e7103b5e6236b911dd347d4c879
douthit.staten <douthit.staten@VA-1116-SC05.local>
2021-10-06 10:45:23 -0700

    Initial commit

---
+++ src/org/vashonsd/Main.java
@@ -0,0 +1,12 @@
+package org.vashonsd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(squareOf(4));
+    }
+    public static int squareOf(int num){
+        return (num * num);
+    }
+
+}
